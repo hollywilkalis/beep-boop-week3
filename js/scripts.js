5c3817111b596ec92e07375385bfba7e04f0585d
Guest User <Guest@Epicodus-9C.local>
2017-10-27 11:05:28 -0700

    Add functioning form submission

---
+++ js/scripts.js
@@ -0,0 +1,17 @@
+//back end logic begins here
+function (conversion(numberToConvert) {
+  var inputArray = numberToConvert.split("");
+
+})
+
+
+
+//front end logic begins here
+$(document).ready(function() {
+  $("form#survey").submit(function(event) {
+    event.preventDefault();
+    var numberToConvert = parseInt($("#inputString").val());
+
+
+  }); //close form submit
+}); //close ready function
